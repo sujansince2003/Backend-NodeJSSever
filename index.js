87a2c6c1b908a358272ba6417c680313e37422c5
sujan khatri <sujansince2059@gmail.com>
2024-04-06 17:11:45 +0545

    global and window object

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const http = require("http");
+// we have global object in node js
+console.log("i am learning node");
+global.console.log("i am learning node");
+// both do the same job
+/**
+ * we can do same for others window objects like setTimeout(),setInterval()....
+ * 
+In browser we can follow same ::::  window.console.log() , window.setInterval() it also works for variables::
+window.name="sujan"
+console.log(name)
+its same as  let name="sujan"
+**but in node the variables we define are not added to the global obj
+they are only scoped to this file only
+this is due to node modular system
+ */
 
-const server = http.createServer((req, res) => {
-  console.log("creating server");
-  res.end("noice");
-});
-server.listen(3002, () => {
-  console.log("server is working d");
-});
+// importing the module
