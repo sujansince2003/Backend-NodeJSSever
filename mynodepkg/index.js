9bd3e8b634fc3843d8b3ba92ab4fc996af6c3667
sujan khatri <sujansince2059@gmail.com>
2024-04-07 19:15:31 +0545

    simple node module and publish to npmJs

---
+++ mynodepkg/index.js
@@ -0,0 +1,7 @@
+module.exports.printName = function (fname) {
+  console.log(`The name is ${fname}`);
+};
+
+module.exports.printCountry = function (fname) {
+  console.log(`Country name is Nepal`);
+};
