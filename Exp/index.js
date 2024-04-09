6abde55a069348e90aacecc6257d083dbbf3e0fd
sujan khatri <sujansince2059@gmail.com>
2024-04-09 20:28:00 +0545

    handling the HTTP PUT Request for updating data  and handling errors

--- Exp/index.js
+++ Exp/index.js
@@ -12,6 +12,12 @@ const express = require("express"); //this returns a function
 const app = express();
 // app is object here
 // app has different methods like app.get(),app.post()...
+/*
+get: to get data
+put: to update data
+post: to add data
+delete:to delete 
+*/
 
 /* app.get()
 it takes two arguments  path and callback function
@@ -38,14 +44,17 @@ app.get("/courses/:cid", (req, res) => {
   res.send(course);
 });
 
-// handling http post request
-app.post("/courses", (req, res) => {
-  // defining schema
+function handleValidate(course) {
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
   });
-  const result = schema.validate(req.body);
+  return schema.validate(course);
+}
 
+// handling http post request
+app.post("/courses", (req, res) => {
+  // defining schema
+  const result = handleValidate(req.body);
   // sending 400 bad request
   if (result.error) {
     res.status(400).send(result.error.details[0].message);
@@ -60,6 +69,29 @@ app.post("/courses", (req, res) => {
   //when server perform post operation then it need to return that obj to the body of response
   res.send(course);
   console.log(courses);
+});
+
+// handling PUT request
+app.put("/courses/:cid", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.cid));
+  // gives course object
+  if (!course) {
+    res.status(404).send("course not available");
+  }
+
+  // const schema = Joi.object({
+  //   name: Joi.string().min(3).required(),
+  // });
+  // const result = schema.validate(req.body);
+  const result = handleValidate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  course.course = req.body.name;
+  // update course property inside course obj
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
