593b124dab058a8eeb16cfd182fa9b22a8e8760e
sujan khatri <sujansince2059@gmail.com>
2024-04-09 20:03:08 +0545

    added Joi for input validation for handling POST requests

--- Exp/index.js
+++ Exp/index.js
@@ -1,4 +1,5 @@
 // making first express server
+const Joi = require("joi");
 
 const courses = [
   { id: 1, course: "CSIT" },
@@ -17,6 +18,9 @@ it takes two arguments  path and callback function
 this callback function is called when we request to that path or url
 callback function takes 2 arguments req and res
 */
+
+//
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello express");
 });
@@ -32,6 +36,30 @@ app.get("/courses/:cid", (req, res) => {
 
   if (!course) res.status(404).send("Data not found");
   res.send(course);
+});
+
+// handling http post request
+app.post("/courses", (req, res) => {
+  // defining schema
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const result = schema.validate(req.body);
+
+  // sending 400 bad request
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    course: req.body.name, // assuming req.body obj has a name property. to make this line work we need to enable working of json parser in body of req
+  };
+  courses.push(course);
+  //when server perform post operation then it need to return that obj to the body of response
+  res.send(course);
+  console.log(courses);
 });
 
 const port = process.env.PORT || 3000;
