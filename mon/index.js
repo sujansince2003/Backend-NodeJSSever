f02b161991c9a2f60d3e56cc5558fd0111fb00ef
sujan khatri <sujansince2059@gmail.com>
2024-04-13 22:06:15 +0545

    creating schema and converting it to model

--- mon/index.js
+++ mon/index.js
@@ -6,12 +6,42 @@ use <dbsNamw>  switch between databases
 show collections   show collections of db
 
 
+basic steps of using mongodb
+schema :define the structure
+  schema -> model
+     model is used for CRUD operations
+
 */
 
 const mongoose = require("mongoose");
+const { unique } = require("underscore");
 mongoose
   .connect("mongodb://localhost/playground")
   .then(() => {
     console.log("connected to mongo");
   })
   .catch((err) => console.log(err));
+
+// defining schemas
+const userSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+  },
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+// schema to model
+//  mongoose.model("nameofModel",userSchema)
+const Usermodel = mongoose.model("usermodel", userSchema);
