80c8d5344bc1e67cad0b5dfa9d9d280041303169
sujan khatri <sujansince2059@gmail.com>
2024-04-13 21:53:33 +0545

    basic mongo cmds

--- mon/index.js
+++ mon/index.js
@@ -1,1 +1,17 @@
+// mongo notes
+/*
+mongosh
+show dbs : show data bases
+use <dbsNamw>  switch between databases
+show collections   show collections of db
+
+
+*/
+
 const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => {
+    console.log("connected to mongo");
+  })
+  .catch((err) => console.log(err));
